61cc8e972601789c1a244a3729ff09f24af77946
Emilien <emilien.schultz@gmail.com>
2024-01-12 17:18:11 +0100

    class SimpleModel in progress

--- activetigger/functions.py
+++ activetigger/functions.py
@@ -1,26 +1,126 @@
 import pandas as pd
+from pandas import DataFrame, Series
 import fasttext
+import spacy
 from sentence_transformers import SentenceTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-def to_fasttext(texts, model = "/home/emilien/models/cc.fr.300.bin"):
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.metrics import precision_score
+
+
+
+# tokenisation ?
+
+
+class SimpleModel():
     """
-    Compute fasttext embeddings
+    Managing fit/predict
+    """
+
+    def __init__(self,
+                 model:str,
+                 data:DataFrame,
+                 label:str,
+                 predictors:list,
+                 standardize:bool=False,
+                 **kwargs
+                 ):
+        
+        # Drop NaN
+        df = data[[label]+predictors].dropna()
+
+        self.Y = df[label]
+        self.X = df[predictors]
+        self.labels = self.Y.unique()
+
+        if standardize:
+            self.standardize()
+
+        # Select model
+        if model == "KNN":
+            self.model = KNeighborsClassifier(n_neighbors=len(self.labels))
+        
+        # Fit model
+        self.model.fit(self.X, self.Y)
+
+        self.precision = self.compute_precision()
+
+    def standardize(self):
+        """
+        Apply standardization
+        """
+        scaler = StandardScaler()
+        self.X = scaler.fit_transform(self.X)
+
+    def compute_precision(self):
+        """
+        Compute precision score
+        """
+        y_pred = self.model.predict(self.X)
+        precision = precision_score(list(self.Y), list(y_pred),pos_label=self.labels[0])
+        return precision
+
+        
+def to_dfm(texts: Series,
+           tfidf:bool=False,
+           ngrams:int=1,
+           min_term_freq:int=5) -> DataFrame:
+    """
+    Compute DFM embedding
+
+    TODO : what is "max_doc_freq" + ADD options from quanteda
+    https://quanteda.io/reference/dfm_tfidf.html
+    """
+
+    if tfidf:
+        vectorizer = TfidfVectorizer(ngram_range=(1, ngrams),
+                                      min_df=min_term_freq)
+    else:
+        vectorizer = CountVectorizer(ngram_range=(1, ngrams),
+                                      min_df=min_term_freq)
+
+    dtm = vectorizer.fit_transform(texts)
+    dtm = pd.DataFrame(dtm.toarray(), 
+                       columns=vectorizer.get_feature_names_out())
+    return dtm
+
+def tokenize(texts: Series,
+             model: str = "fr_core_news_sm")->Series:
+    """
+    Clean texts with tokenization to facilitate word count
+    """
+
+    nlp = spacy.load(model)
+    def tokenize(t,nlp):
+        return " ".join([str(token) for token in nlp.tokenizer(t)])
+    textes_tk = texts.apply(lambda x : tokenize(x,nlp))
+    return textes_tk
+
+def to_fasttext(texts: Series,
+                 model: str = "/home/emilien/models/cc.fr.300.bin") -> DataFrame:
+    """
+    Compute fasttext embedding
     Args:
         texts (pandas.Series): texts
         model (str): model to use
     Returns:
         pandas.DataFrame: embeddings
     """
-
+    texts_tk = tokenize(texts)
     ft = fasttext.load_model(model)
-    emb = [ft.get_sentence_vector(t.replace("\n"," ")) for t in texts]
-    emb = pd.DataFrame(emb,index=texts.index)
-    emb.columns = ["ft%03d" % (x + 1) for x in range(len(emb.columns))]
-    return emb
+    emb = [ft.get_sentence_vector(t.replace("\n"," ")) for t in texts_tk]
+    df = pd.DataFrame(emb,index=texts.index)
+    df.columns = ["ft%03d" % (x + 1) for x in range(len(df.columns))]
+    return df
 
-def to_sbert(texts, model = "distiluse-base-multilingual-cased-v1"):
+def to_sbert(texts: Series, 
+             model:str = "distiluse-base-multilingual-cased-v1") -> DataFrame:
     """
-    Compute sbert embeddings
+    Compute sbert embedding
     Args:
         texts (pandas.Series): texts
         model (str): model to use
@@ -33,3 +133,4 @@ def to_sbert(texts, model = "distiluse-base-multilingual-cased-v1"):
     emb = pd.DataFrame(emb,index=texts.index)
     emb.columns = ["sb%03d" % (x + 1) for x in range(len(emb.columns))]
     return emb
+
