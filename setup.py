4199617fe9b855e8f7767a6118cb3f8cfdb9f6a6
Emilien <emilien.schultz@gmail.com>
2024-03-20 21:47:49 +0100

    modify server variables

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Activetigger',
-      version='0.0.8',
+      version='0.0.9',
       description='Active Tigger in Python',
       author='Émilien Schultz',
       author_email='emilien.schultz@gmail.com',
