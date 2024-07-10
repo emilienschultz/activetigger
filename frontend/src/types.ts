0217b8ea64c967d9eff96a99221bb22c5128f1d5
Paul Girard <paul@ouestware.com>
2024-07-10 17:00:44 +0200

    [frontend] core API code documentation

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,5 +1,12 @@
 import { components } from './generated/openapi';
 
+/**
+ * API data types
+ * this file rename API data types from the generated ones to make our code cleaner
+ * Types are generated in a components['schemas'] map which is hideous
+ * we extract what we use little by little when needed
+ */
+
 export type UserModel = components['schemas']['UserModel'];
 
 export type ProjectModel = components['schemas']['ProjectModel'];
@@ -9,3 +16,4 @@ export type AvailableProjectsModel = {
   created_at: string;
   parameters: ProjectModel;
 };
+export type LoginParams = components['schemas']['Body_login_for_access_token_token_post'];
