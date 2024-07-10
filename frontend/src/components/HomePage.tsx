70299e673199803bfda7caf8d58a34d9961f9866
Paul Girard <paul@ouestware.com>
2024-07-10 12:33:30 +0200

    [frontend] loggedin user in home page relates #64

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useAuth } from '../core/auth';
 import { LoginForm } from './forms/LoginForm';
 
 export const HomePage: FC = () => {
+  const { authenticatedUser } = useAuth();
   return (
     <main className="container-fluid">
       <div className="row">
@@ -11,7 +13,16 @@ export const HomePage: FC = () => {
         <div className="col-12 col-lg-6">
           <h1>Active tigger</h1>
           <h3>Explore & Annotate textual data</h3>
-          <LoginForm />
+          {!authenticatedUser ? (
+            <LoginForm />
+          ) : (
+            <div>
+              <p>
+                you're logged in as <b>{authenticatedUser.username}</b> ({authenticatedUser.status})
+              </p>
+              <Link to="/projects">your projects</Link>
+            </div>
+          )}
           <div className="general-info">
             <div>Frontend v0.1</div>
             <div>For any information, please contact emilien.schultz [at] ensae.fr</div>
