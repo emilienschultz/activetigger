ff9ff5afd0bc1b3bde6f8d28ec26c8a1b86e384d
Emilien Schultz <emilien.schultz@gmail.com>
2024-07-16 10:38:37 +0200

    refactor scheme_management component

--- frontend/src/components/forms/SchemesManagementForms.tsx
+++ frontend/src/components/forms/SchemesManagementForms.tsx
@@ -14,28 +14,28 @@ interface SchemesManagementProps {
   reFetchProject: () => void;
 }
 
-/* Manage schemes*/
+/* Manage schemes
+ * Select ; Delete ; Add
+ */
+
 export const SchemesManagement: FC<SchemesManagementProps> = ({
   available_schemes,
   projectSlug,
   reFetchProject,
 }) => {
+  // manage context
   const {
-    appContext: { current_scheme }, //destructuration of the object from hook
+    appContext: { current_scheme },
     setAppContext,
   } = useAppContext();
 
+  // hooks to use the objets
+  const { register, handleSubmit } = useForm<SchemeModel>({});
+  const addScheme = useAddScheme(projectSlug);
+  const { notify } = useNotifications();
+
   // hook to get the api call
   const deleteScheme = useDeleteScheme(projectSlug, current_scheme);
-
-  // put the current scheme in the context
-  const handleChange = (event: any) => {
-    setAppContext((state) => ({
-      ...state,
-      current_scheme: event.target.value,
-    }));
-    console.log(`Current scheme ${event.target.value}`);
-  };
 
   // state for displaying the new scheme menu
   const [showCreateNewScheme, setShowCreateNewScheme] = useState(false);
@@ -43,78 +43,83 @@ export const SchemesManagement: FC<SchemesManagementProps> = ({
     setShowCreateNewScheme(!showCreateNewScheme);
   };
 
-  return (
-    <div>
-      <label className="form-label" htmlFor="scheme-selected">
-        Select current scheme
-      </label>
-      <div>
-        <select id="scheme-selected" className="form-select-lg mb-3 col-3" onChange={handleChange}>
-          <option></option> {/*empty possibility*/}
-          {available_schemes.map((element) => (
-            <option key={element} value={element}>
-              {element}
-            </option>
-          ))}{' '}
-        </select>
-        <button
-          onClick={async () => {
-            //TODO: try catch and throw
-            await deleteScheme();
-            reFetchProject();
-          }}
-          className="btn"
-        >
-          <MdOutlineDeleteOutline size={30} />
-        </button>
-        <button onClick={handleIconClick} className="btn">
-          <IoIosAddCircle size={30} />
-        </button>
-      </div>
-      <div>
-        {showCreateNewScheme && (
-          <CreateNewScheme projectSlug={projectSlug} reFetchProject={reFetchProject} />
-        )}
-      </div>
-    </div>
-  );
-};
+  // put the current scheme in the context on change
+  const handleSelectScheme = (event: any) => {
+    setAppContext((state) => ({
+      ...state,
+      current_scheme: event.target.value,
+    }));
+    console.log(`Current scheme ${event.target.value}`);
+  };
 
-interface CreateNewSchemeProps {
-  projectSlug: string;
-  reFetchProject: () => void;
-}
-
-/* New Scheme creation */
-export const CreateNewScheme: FC<CreateNewSchemeProps> = ({ projectSlug, reFetchProject }) => {
-  // hooks to use the objets
-  const { register, handleSubmit } = useForm<SchemeModel>({});
-  const addScheme = useAddScheme(projectSlug);
-  const { notify } = useNotifications();
-
-  // action when form validated
-  const onSubmit: SubmitHandler<SchemeModel> = async (formData) => {
+  // action to create the new scheme
+  const createNewScheme: SubmitHandler<SchemeModel> = async (formData) => {
     try {
       addScheme(formData.name);
-      notify({ type: 'success', message: 'Scheme created' });
-      reFetchProject();
+      notify({ type: 'success', message: `Scheme ${formData.name} created` });
     } catch (error) {
       notify({ type: 'error', message: error + '' });
     }
+    reFetchProject();
+    setShowCreateNewScheme(!showCreateNewScheme);
+  };
+
+  // action to delete scheme
+  const deleteSelectedScheme = async () => {
+    //TODO: try catch and throw
+    await deleteScheme();
+    reFetchProject();
   };
 
   return (
     <div className="container-fluid">
       <div className="row">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="secondary-panel col-4">
-            <label className="form-label" htmlFor="scheme_name">
-              New scheme
-            </label>
-            <input className="form-control" id="scheme_name" type="text" {...register('name')} />
-            <button className="btn btn-primary btn-validation">Create</button>
-          </div>
-        </form>
+        <label className="form-label" htmlFor="scheme-selected">
+          Select current scheme
+        </label>
+        <div>
+          <select
+            id="scheme-selected"
+            className="form-select-lg col-3 mb-3"
+            onChange={handleSelectScheme}
+          >
+            <option></option> {/*empty possibility*/}
+            {available_schemes.map((element) => (
+              <option key={element} value={element}>
+                {element}
+              </option>
+            ))}{' '}
+          </select>
+          <button onClick={deleteSelectedScheme} className="btn btn p-0">
+            <MdOutlineDeleteOutline size={30} />
+          </button>
+          <button onClick={handleIconClick} className="btn p-0">
+            <IoIosAddCircle size={30} />
+          </button>
+        </div>
+      </div>
+      <div>
+        {
+          // only display if click on the add button
+          showCreateNewScheme && (
+            <div className="row">
+              <form onSubmit={handleSubmit(createNewScheme)}>
+                <div className="secondary-panel col-4">
+                  <label className="form-label" htmlFor="scheme_name">
+                    New scheme
+                  </label>
+                  <input
+                    className="form-control"
+                    id="scheme_name"
+                    type="text"
+                    {...register('name')}
+                  />
+                  <button className="btn btn-primary btn-validation">Create</button>
+                </div>
+              </form>
+            </div>
+          )
+        }
       </div>
     </div>
   );
