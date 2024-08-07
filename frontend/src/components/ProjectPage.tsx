6bee3cc5808a2d59630de41df1e53a995e55dc96
Emilien Schultz <emilien.schultz@gmail.com>
2024-08-07 18:08:53 +0200

    move statistics component in specific file

--- frontend/src/components/ProjectPage.tsx
+++ frontend/src/components/ProjectPage.tsx
@@ -1,68 +1,12 @@
 import { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { useProject, useStatistics } from '../core/api';
+import { useProject } from '../core/api';
 import { useAppContext } from '../core/context';
+import { ProjectStatistics } from './ProjectStatistics';
 import { FeaturesManagement } from './forms/FeaturesManagementForm';
 import { SchemesManagement } from './forms/SchemesManagementForm';
 import { ProjectPageLayout } from './layout/ProjectPageLayout';
-
-interface StatisticsProps {
-  projectSlug: string;
-  scheme: string;
-}
-
-/**
- * Component to display statistics
- */
-const DisplayStatistics: FC<StatisticsProps> = ({ projectSlug, scheme }) => {
-  const { statistics } = useStatistics(projectSlug, scheme);
-
-  if (!statistics) return null;
-
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="subsection">Statistics</div>
-          <table className="table-statistics">
-            <tbody>
-              <tr className="table-delimiter">
-                <td>Trainset</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Total</td>
-                <td>{statistics['trainset_n']}</td>
-              </tr>
-              <tr>
-                <td>Annotated</td>
-                <td>{statistics['annotated_n']}</td>
-              </tr>
-              <tr>
-                <td>Users involved</td>
-                <td>{statistics['users']}</td>
-              </tr>
-              <tr>
-                <td>Distribution</td>
-                <td>{JSON.stringify(statistics['annotated_distribution'])}</td>
-              </tr>
-              <tr className="table-delimiter">
-                <td>Testset</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>Total</td>
-                <td>{statistics['testset_n']}</td>
-              </tr>
-            </tbody>
-          </table>
-          {/*JSON.stringify(statistics, null, 2)*/}
-        </div>
-      </div>
-    </div>
-  );
-};
 
 /**
  * Component to display the project page
@@ -89,16 +33,6 @@ export const ProjectPage: FC = () => {
 
   const { reFetch } = useProject(projectName); // get project statefrom the API
 
-  // // Effect to poll project data regularly to monitor long lasting server tasks
-  // useEffect(() => {
-  //   // execute a fetch call to update project data every 2000ms
-  //   const intervalId = setInterval(reFetch, 2000);
-  //   // useEffect can return a method which is executed when the component is unmounted
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, [reFetch]);
-
   return (
     <ProjectPageLayout projectName={projectName}>
       {project && (
@@ -118,7 +52,7 @@ export const ProjectPage: FC = () => {
               possibleFeatures={project.features.options}
             />
           </div>
-          {currentScheme && <DisplayStatistics projectSlug={projectName} scheme={currentScheme} />}
+          {currentScheme && <ProjectStatistics projectSlug={projectName} scheme={currentScheme} />}
         </div>
       )}
     </ProjectPageLayout>
