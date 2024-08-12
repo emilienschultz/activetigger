2d11296df7f66459a1cc4b66f11a532058421d32
Emilien Schultz <emilien.schultz@gmail.com>
2024-08-12 11:48:02 +0200

    add under computation display

--- frontend/src/components/layout/ProjectActionsSideBar.tsx
+++ frontend/src/components/layout/ProjectActionsSideBar.tsx
@@ -1,18 +1,30 @@
 import classNames from 'classnames';
 import { FC } from 'react';
 import { FaCogs } from 'react-icons/fa';
+import { FaGear } from 'react-icons/fa6';
 import { MdOutlineTransform } from 'react-icons/md';
 import { PiTagDuotone } from 'react-icons/pi';
 import { Link } from 'react-router-dom';
 
+import { ProjectStateModel } from '../../types';
 import { PossibleProjectActions } from './ProjectPageLayout';
 
 /* define a component for project action bar 
 with the project & the current action*/
 export const ProjectActionsSidebar: FC<{
-  projectName: string;
+  projectState: ProjectStateModel;
   currentProjectAction?: PossibleProjectActions;
-}> = ({ currentProjectAction, projectName }) => {
+  currentScheme?: string;
+  currentUser: string;
+}> = ({ currentProjectAction, projectState, currentScheme, currentUser }) => {
+  const projectName = projectState.params.project_slug;
+
+  // test if computation is currently undergoing
+  const currentComputation =
+    Array(projectState.bertmodels.training).includes(currentUser) ||
+    Array(projectState.simplemodel.training).includes(currentUser) ||
+    (projectState.features.training as string[]).length > 0;
+
   return (
     <div className="project-sidebar d-flex flex-column flex-shrink-0 p-3 bg-light">
       <ul className="nav nav-pills flex-column mb-auto">
@@ -57,6 +69,15 @@ export const ProjectActionsSidebar: FC<{
             </span>
           </Link>
         </li>
+        {currentComputation && (
+          <li className="nav-item ">
+            <div className="nav-link computing">
+              <span className="computing">
+                <FaGear size={20} /> Computing
+              </span>
+            </div>
+          </li>
+        )}
       </ul>
     </div>
   );
--- frontend/src/components/layout/ProjectPageLayout.tsx
+++ frontend/src/components/layout/ProjectPageLayout.tsx
@@ -1,5 +1,7 @@
 import { FC, PropsWithChildren } from 'react';
 
+import { useAuth } from '../../core/auth';
+import { useAppContext } from '../../core/context';
 import { PageLayout } from './PageLayout';
 import { ProjectActionsSidebar } from './ProjectActionsSideBar';
 
@@ -16,12 +18,26 @@ export const ProjectPageLayout: FC<ProjectPageLayoutProps> = ({
   currentAction,
   children,
 }) => {
+  // get the current state of the project
+  const {
+    appContext: { currentProject: project, currentScheme },
+  } = useAppContext();
+  if (!project) return '';
+  // get current user
+  const { authenticatedUser } = useAuth();
+  if (!authenticatedUser) return '';
+
   return (
     <PageLayout currentPage="projects" projectName={projectName}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-3">
-            <ProjectActionsSidebar projectName={projectName} currentProjectAction={currentAction} />
+            <ProjectActionsSidebar
+              projectState={project}
+              currentProjectAction={currentAction}
+              currentScheme={currentScheme}
+              currentUser={authenticatedUser.username}
+            />
           </div>
           <div className="col-9">{children}</div>
         </div>
