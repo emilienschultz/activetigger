ce691d68fc5ad8393423aba1f57b0c91b3d66e0b
Emilien Schultz <emilien.schultz@gmail.com>
2024-08-13 19:43:05 +0200

    prettify user page

--- frontend/src/components/UsersPage.tsx
+++ frontend/src/components/UsersPage.tsx
@@ -45,154 +45,134 @@ export const UsersPage: FC = () => {
     <PageLayout currentPage="users">
       <div className="container-fluid">
         <div className="row">
+          <div className="col-2"></div>
           <div className="col-6">
             <h2 className="subsection">Manage users and auth</h2>
             <span className="explanations">
               Create or delete users and authorization to projects
             </span>
-            <div className="row">
-              <h4 className="subsection">Users</h4>
-            </div>
-            <div className="row">
-              <div className="col-4 d-flex align-items-center">
-                <select
-                  id="select-user"
-                  className="form-select"
-                  onChange={(e) => {
-                    setCurrentUser(e.target.value);
-                  }}
-                >
-                  <option></option>
+            <h4 className="subsection">Users</h4>
+            <div className="explanations">Select a user to act on it</div>
 
-                  {users && users.map((e) => <option key={e}>{e}</option>)}
+            <div className="d-flex align-items-center">
+              <select
+                id="select-user"
+                className="form-select"
+                onChange={(e) => {
+                  setCurrentUser(e.target.value);
+                }}
+              >
+                <option></option>
+
+                {users && users.map((e) => <option key={e}>{e}</option>)}
+              </select>
+              <button
+                className="btn btn p-0"
+                onClick={() => {
+                  deleteUser(currentUser);
+                  reFetchUsers();
+                }}
+              >
+                <MdOutlineDeleteOutline size={30} />
+              </button>
+            </div>
+            <details className="custom-details">
+              <summary>Create user</summary>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  className="form-control me-2 mt-2"
+                  type="text"
+                  {...register('username')}
+                  placeholder="New user name"
+                />
+                <input
+                  className="form-control me-2 mt-2"
+                  type="text"
+                  {...register('password')}
+                  placeholder="Password"
+                />
+                <select {...register('status')} className="me-2 mt-2">
+                  <option>manager</option>
+                  <option>annotator</option>
                 </select>
-                <button
-                  className="btn btn p-0"
-                  onClick={() => {
-                    deleteUser(currentUser);
-                    reFetchUsers();
-                  }}
-                >
-                  <MdOutlineDeleteOutline size={30} />
-                </button>
-              </div>
-            </div>
-            <div className="row">
-              <details>
-                <summary>Create user</summary>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <div className="d-flex align-items-center">
-                    <input
-                      className="form-control me-2 mt-2"
-                      type="text"
-                      {...register('username')}
-                      placeholder="New user name"
-                    />
-                    <input
-                      className="form-control me-2 mt-2"
-                      type="text"
-                      {...register('password')}
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div className="col-3">
-                    <select {...register('status')} className="me-2 mt-2">
-                      <option>manager</option>
-                      <option>annotator</option>
-                    </select>
-                    <button className="btn btn-primary me-2 mt-2">Create user</button>
-                  </div>
-                </form>
-              </details>
-            </div>
-            <div className="row">
-              <h4 className="subsection">Rights</h4>
-            </div>
-            <div className="row">
-              <div className="col-8">
-                <span className="explanations">Select the project to see authorizations</span>
+                <button className="btn btn-primary me-2 mt-2">Create user</button>
+              </form>
+            </details>
+            <h4 className="subsection">Rights</h4>
+            <span className="explanations">Select the project to see authorizations</span>
 
-                <br></br>
-                {
-                  <select
-                    className="form-select"
-                    onChange={(e) => {
-                      setCurrentProjectSlug(e.target.value);
-                    }}
-                  >
-                    <option></option>
-                    {(projects || []).map((project) => (
-                      <option key={project.parameters.project_slug}>
-                        {project.parameters.project_slug}
-                      </option>
-                    ))}
-                  </select>
-                }
-                <div>
-                  {authUsers ? (
-                    <table>
-                      <tr>
-                        <th>User</th>
-                        <th>Auth</th>
-                        <th>Delete</th>
-                      </tr>
-                      {Object.entries(authUsers).map(([user, auth]) => (
-                        <tr>
-                          <td>{user}</td>
-                          <td>{auth}</td>
-                          <td>
-                            <button
-                              className="btn btn p-0"
-                              onClick={() => {
-                                deleteUserAuth(user);
-                              }}
-                            >
-                              <MdOutlineDeleteOutline />
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </table>
-                  ) : (
-                    <span></span>
-                  )}
-                </div>
-              </div>
+            <br></br>
+            {
+              <select
+                className="form-select"
+                onChange={(e) => {
+                  setCurrentProjectSlug(e.target.value);
+                }}
+              >
+                <option></option>
+                {(projects || []).map((project) => (
+                  <option key={project.parameters.project_slug}>
+                    {project.parameters.project_slug}
+                  </option>
+                ))}
+              </select>
+            }
+            <div>
+              {authUsers ? (
+                <table className="table-auth">
+                  <tr>
+                    <th>User</th>
+                    <th>Auth</th>
+                    <th>Delete</th>
+                  </tr>
+                  {Object.entries(authUsers).map(([user, auth]) => (
+                    <tr>
+                      <td>{user}</td>
+                      <td>{auth}</td>
+                      <td>
+                        <button
+                          className="btn btn p-0"
+                          onClick={() => {
+                            deleteUserAuth(user);
+                          }}
+                        >
+                          <MdOutlineDeleteOutline />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </table>
+              ) : (
+                <span></span>
+              )}
             </div>
-            <div className="row">
-              <div className="col-8">
-                <details>
-                  <summary>Add authorization</summary>
-                  <div className="d-flex align-items-center">
-                    <div className="row">
-                      <select
-                        id="select-auth"
-                        className="form-select"
-                        onChange={(e) => {
-                          setCurrentAuth(e.target.value);
-                        }}
-                      >
-                        <option>manager</option>
-                        <option>annotator</option>
-                      </select>
-                      <button
-                        onClick={() => {
-                          console.log(currentUser);
-                          console.log(currentAuth);
-                          if (currentUser && currentAuth) {
-                            addUserAuth(currentUser, currentAuth);
-                          }
-                          reFetchUsers();
-                        }}
-                        className="btn btn-primary me-2 mt-2"
-                      >
-                        Add rights
-                      </button>
-                    </div>
-                  </div>
-                </details>
-              </div>
-            </div>
+
+            <details className="custom-details">
+              <summary>Add authorization</summary>
+              <select
+                id="select-auth"
+                className="form-select"
+                onChange={(e) => {
+                  setCurrentAuth(e.target.value);
+                }}
+              >
+                <option>manager</option>
+                <option>annotator</option>
+              </select>
+              <button
+                onClick={() => {
+                  console.log(currentUser);
+                  console.log(currentAuth);
+                  if (currentUser && currentAuth) {
+                    addUserAuth(currentUser, currentAuth);
+                  }
+                  reFetchUsers();
+                }}
+                className="btn btn-primary me-2 mt-2"
+              >
+                Add rights
+              </button>
+            </details>
           </div>
         </div>
       </div>
