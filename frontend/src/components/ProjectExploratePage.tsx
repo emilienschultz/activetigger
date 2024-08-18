7b2c1e7f0ebca5916e2ad191ecc8bd0c37c81588
Emilien Schultz <emilien.schultz@gmail.com>
2024-08-18 10:23:46 +0200

    [frontend] advance exploratory table

--- frontend/src/components/ProjectExploratePage.tsx
+++ frontend/src/components/ProjectExploratePage.tsx
@@ -1,10 +1,7 @@
-import { ColDef, ColGroupDef } from 'ag-grid-community';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
-import { AgGridReact } from 'ag-grid-react';
-import { FC, useState } from 'react';
-import DataGrid, { RenderEditCellProps, SelectColumn, textEditor } from 'react-data-grid';
-import type { Row } from 'react-data-grid';
+import { FC, useEffect, useState } from 'react';
+import DataGrid, { Column, RenderEditCellProps, SelectColumn, textEditor } from 'react-data-grid';
 import 'react-data-grid/lib/styles.css';
 import { useParams } from 'react-router-dom';
 
@@ -13,57 +10,75 @@ import { useAppContext } from '../core/context';
 import { ProjectPageLayout } from './layout/ProjectPageLayout';
 
 /**
- * Component to display the features page
+ * Component to display the exploratory page
  */
 
-const labels = ['Dr.', 'Mr.', 'Mrs.', 'Miss', 'Ms.'] as const;
-
-export function renderDropdown({ row, onRowChange }: RenderEditCellProps<Row>) {
-  return (
-    <select
-      value={row.labels}
-      onChange={(event) => onRowChange({ ...row, labels: event.target.value }, true)}
-      autoFocus
-    >
-      {labels.map((l) => (
-        <option key={l} value={l}>
-          {l}
-        </option>
-      ))}
-    </select>
-  );
+interface Row {
+  index: string;
+  timestamp: string;
+  labels: string;
+  text: string;
 }
 
 export const ProjectExploratePage: FC = () => {
   const { projectName } = useParams();
   if (!projectName) return null;
   const {
-    appContext: { currentScheme },
-    setAppContext,
+    appContext: { currentScheme, currentProject: project },
   } = useAppContext();
   if (!currentScheme) return null;
-  const [rowData, setRowData] = useState([]);
+  if (!project) return null;
+
+  const availableLabels =
+    currentScheme && project ? project.schemes.available[currentScheme] || [] : [];
   const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
   const [pageSize, setPageSize] = useState(20);
 
-  const [loading, setLoading] = useState(false);
+  // get API elements when table shape change
   const { table } = useGetTableElements(
     projectName,
     currentScheme,
     page * pageSize,
     (page + 1) * pageSize,
   );
-  console.log(table);
-  console.log(pageSize);
-  console.log(page);
 
-  const columns = [
+  const [rows, setRows] = useState(table ? table['items'] : []);
+
+  // update rows only when a even trigger the update table
+  useEffect(() => {
+    if (table) {
+      setRows(table['items']);
+      console.log('updage table');
+    }
+  }, [table]);
+
+  // define table
+  const columns: readonly Column<Row>[] = [
     { key: 'index', name: 'ID', resizable: true },
     { key: 'timestamp', name: 'Timestamp', resizable: true },
     { key: 'labels', name: 'Label', resizable: true, renderEditCell: renderDropdown },
     { key: 'text', name: 'Text', resizable: true },
   ];
+
+  // specific function to have a select component
+  function renderDropdown({ row, onRowChange }: RenderEditCellProps<Row>) {
+    return (
+      <select
+        value={row.labels}
+        onChange={(event) => {
+          onRowChange({ ...row, labels: event.target.value }, true);
+          console.log(event.target.value);
+        }}
+        autoFocus
+      >
+        {availableLabels.map((l) => (
+          <option key={l} value={l}>
+            {l}
+          </option>
+        ))}
+      </select>
+    );
+  }
 
   return (
     <ProjectPageLayout projectName={projectName} currentAction="explorate">
@@ -72,7 +87,6 @@ export const ProjectExploratePage: FC = () => {
           <div className="col-0"></div>
           <div className="col-10">
             <h2 className="subsection">Data exploration</h2>
-
             {table && (
               <div>
                 <div className="d-flex align-items-center justify-content-between mb-3">
@@ -94,12 +108,19 @@ export const ProjectExploratePage: FC = () => {
                     }}
                   />
                 </div>
-                <div>
-                  <DataGrid columns={columns} rows={table['items'] ? table['items'] : []} />
+                <div className="rdg rnvodz5 fill-grid">
+                  <DataGrid
+                    columns={columns}
+                    rows={rows}
+                    onRowsChange={(e) => {
+                      setRows(e);
+                      console.log(e);
+                    }}
+                  />
                 </div>
               </div>
             )}
-          </div>{' '}
+          </div>
         </div>
       </div>
     </ProjectPageLayout>
