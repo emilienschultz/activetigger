f7569570e465c18d1417bb8e0ebb55978b9ede28
Emilien Schultz <emilien.schultz@gmail.com>
2024-08-23 12:04:58 +0200

    [api] remove streamlit frontend

--- activetigger/__main__.py
+++ activetigger/__main__.py
@@ -2,45 +2,46 @@
 import uvicorn
 import argparse
 from pathlib import Path
-import subprocess
-import importlib.util
+
+# import subprocess
+# import importlib.util
 
 
-def get_streamlit_path():
-    """
-    Look for path
-    """
-    if Path("./activetigger/frontend.py").exists():
-        return "./activetigger/frontend.py"
-    lib = importlib.util.find_spec("activetigger")
-    if lib:
-        path = lib.origin.replace("/__init__.py", "frontend.py")
-        if Path(path).exists():
-            return path
-    return None
+# def get_streamlit_path():
+#     """
+#     Look for path
+#     """
+#     if Path("./activetigger/frontend.py").exists():
+#         return "./activetigger/frontend.py"
+#     lib = importlib.util.find_spec("activetigger")
+#     if lib:
+#         path = lib.origin.replace("/__init__.py", "frontend.py")
+#         if Path(path).exists():
+#             return path
+#     return None
 
 
 if __name__ == "__main__":
     """
     Launch the service
     - launch FASTAPI backend
-    - launch Streamlit app
     """
     parser = argparse.ArgumentParser(description="Run pyActiveTigger")
-    parser.add_argument(
-        "-s",
-        "--server",
-        type=bool,
-        default=False,
-        help="Server only (FASTAPI). Default is False.",
-    )
-    parser.add_argument(
-        "-a",
-        "--adress",
-        type=str,
-        default="0.0.0.0",
-        help='IP address the application will listen on. Default is "0.0.0.0".',
-    )
+
+    # parser.add_argument(
+    #     "-s",
+    #     "--server",
+    #     type=bool,
+    #     default=False,
+    #     help="Server only (FASTAPI). Default is False.",
+    # )
+    # parser.add_argument(
+    #     "-a",
+    #     "--adress",
+    #     type=str,
+    #     default="0.0.0.0",
+    #     help='IP address the application will listen on. Default is "0.0.0.0".',
+    # )
     parser.add_argument(
         "-p",
         "--portapi",
@@ -48,13 +49,13 @@ if __name__ == "__main__":
         default=5000,
         help="Port number for the API. Default is 5000.",
     )
-    parser.add_argument(
-        "-q",
-        "--portfront",
-        type=int,
-        default=8000,
-        help="Port number for the API. Default is 8000.",
-    )
+    # parser.add_argument(
+    #     "-q",
+    #     "--portfront",
+    #     type=int,
+    #     default=8000,
+    #     help="Port number for the API. Default is 8000.",
+    # )
     parser.add_argument(
         "-c",
         "--config",
@@ -74,27 +75,28 @@ path_models: ./models
         with open("./config.yaml", "w") as f:
             f.write(content)
 
-    # launch streamlit app
-    if not args.server:
-        print("Start streamlit app")
-        path = get_streamlit_path()
-        if path is None:
-            print("Error finding the streamlit app")
-        else:
-            process = subprocess.Popen(
-                [
-                    "python",
-                    "-m" "streamlit",
-                    "run",
-                    path,
-                    "--server.address",
-                    args.adress,
-                    "--server.port",
-                    str(args.portfront),
-                ]
-            )
+    # # launch streamlit app
+    # if not args.server:
+    #     print("Start streamlit app")
+    #     path = get_streamlit_path()
+    #     if path is None:
+    #         print("Error finding the streamlit app")
+    #     else:
+    #         process = subprocess.Popen(
+    #             [
+    #                 "python",
+    #                 "-m" "streamlit",
+    #                 "run",
+    #                 path,
+    #                 "--server.address",
+    #                 args.adress,
+    #                 "--server.port",
+    #                 str(args.portfront),
+    #             ]
+    #         )
 
     # launch fastapi app
+
     print("Streamlit app launched")
     uvicorn.run(
         "activetigger.api:app", host=args.adress, port=args.portapi, reload=True
