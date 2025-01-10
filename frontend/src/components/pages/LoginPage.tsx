4d50561b2572450ded13dae05a8fa7f9afc63154
Émilien Schultz <emilien.schultz@gmail.com>
2025-01-10 18:07:22 +0100

    [frontend] add account menu

---
+++ frontend/src/components/pages/LoginPage.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { useAuth } from '../../core/auth';
+import { LoginForm } from '../forms/LoginForm';
+import { PageLayout } from '../layout/PageLayout';
+
+export const LoginPage: FC = () => {
+  const { authenticatedUser } = useAuth();
+  const { state } = useLocation();
+
+  return (
+    <PageLayout currentPage="login">
+      <div className="container-fluid">
+        {authenticatedUser?.username && (
+          <div className="row">
+            <div className="col-1"></div>
+
+            <div className="col-11 col-lg-6 ">
+              <div className="user-info">
+                You're logged in as <span>{authenticatedUser.username}</span> ( status :{' '}
+                {authenticatedUser.status})
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="row">
+          <div className="col-1"></div>
+
+          <div className="col-11 col-lg-6">
+            <div className="subsection m-2">Change account</div>
+
+            <LoginForm redirectTo={state?.path || '/projects'} />
+          </div>
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
