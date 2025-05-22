ea56d73bbeb3bf2e70a2e74e8a49d349c11116e1
Émilien Schultz <emilien.schultz@gmail.com>
2025-05-22 08:39:43 +0200

    refacto projectpage

---
+++ frontend/src/components/ProjectHistory.tsx
@@ -0,0 +1,82 @@
+import { Dispatch, FC, SetStateAction } from 'react';
+
+import { useGetLogs } from '../core/api';
+
+import DataGrid, { Column } from 'react-data-grid';
+import { HiOutlineQuestionMarkCircle } from 'react-icons/hi';
+import { Tooltip } from 'react-tooltip';
+import { AppContextValue } from '../core/context';
+
+interface ProjectHistoryProps {
+  projectSlug: string;
+  history: string[];
+  setAppContext: Dispatch<SetStateAction<AppContextValue>>;
+}
+
+interface Row {
+  time: string;
+  user: string;
+  action: string;
+}
+
+export const ProjectHistory: FC<ProjectHistoryProps> = ({
+  projectSlug,
+  history,
+  setAppContext,
+}) => {
+  // get logs
+  const { logs } = useGetLogs(projectSlug, 100);
+
+  // function to clear history
+  const actionClearHistory = () => {
+    setAppContext((prev) => ({ ...prev, history: [] }));
+  };
+
+  const columns: readonly Column<Row>[] = [
+    {
+      name: 'Time',
+      key: 'time',
+      resizable: true,
+    },
+    {
+      name: 'User',
+      key: 'user',
+      resizable: true,
+    },
+    {
+      name: 'Project',
+      key: 'project',
+    },
+    {
+      name: 'Action',
+      key: 'action',
+    },
+  ];
+  return (
+    <div>
+      <span className="explanations">History of the current session</span>
+      <div>
+        Session counter{' '}
+        <a className="history">
+          <HiOutlineQuestionMarkCircle />
+        </a>
+        <Tooltip anchorSelect=".history" place="top">
+          Element annotated during this session. If you annotate already annotated data, it prevents
+          you to see an element twice. Clear it if you want to be able to re-annotate again.
+        </Tooltip>{' '}
+        <span className="badge rounded-pill text-bg-light text-muted me-2" key={history.length}>
+          {history.length}
+        </span>
+      </div>
+      <button onClick={actionClearHistory} className="delete-button">
+        Clear history
+      </button>
+      <div className="subsection">Activity on this project</div>
+      <DataGrid<Row>
+        className="fill-grid mt-2"
+        columns={columns}
+        rows={(logs as unknown as Row[]) || []}
+      />
+    </div>
+  );
+};
---
+++ frontend/src/components/ProjectParameters.tsx
@@ -0,0 +1,104 @@
+import { FC, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { useNavigate } from 'react-router-dom';
+import { useDeleteProject } from '../core/api';
+import { ProjectStateModel } from '../types';
+import { ProjectUpdateForm } from './forms/ProjectUpdateForm';
+
+export interface ProjectParametersModel {
+  project: ProjectStateModel;
+  projectSlug: string;
+}
+
+export const ProjectParameters: FC<ProjectParametersModel> = ({ project, projectSlug }) => {
+  const navigate = useNavigate();
+
+  // modals to delete
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // show modify
+  const [showModify, setShowModify] = useState(false);
+
+  // function to delete project
+  const deleteProject = useDeleteProject();
+  const actionDelete = async () => {
+    if (projectSlug) {
+      await deleteProject(projectSlug);
+      navigate(`/projects/`);
+    }
+  };
+
+  return (
+    <>
+      <div className="explanations">Parameters of this project</div>
+      <button className="btn btn-primary" onClick={() => setShowModify(!showModify)}>
+        Change parameters
+      </button>
+
+      {showModify && <ProjectUpdateForm />}
+      <table className="table-statistics">
+        <tbody>
+          <tr className="table-delimiter">
+            <td>Parameters</td>
+            <td>Value</td>
+          </tr>
+          <tr>
+            <td>Project name</td>
+            <td>{project.params.project_name}</td>
+          </tr>
+          <tr>
+            <td>Project slug</td>
+            <td>{project.params.project_slug}</td>
+          </tr>
+          <tr>
+            <td>Filename</td>
+            <td>{project.params.filename}</td>
+          </tr>
+          <tr>
+            <td>Total rows file</td>
+            <td>{project.params.n_total}</td>
+          </tr>
+          <tr>
+            <td>Language</td>
+            <td>{project.params.language}</td>
+          </tr>
+          <tr>
+            <td>Columns text</td>
+            <td>{project.params.cols_text}</td>
+          </tr>
+          <tr>
+            <td>Column id</td>
+            <td>{project.params.col_id}</td>
+          </tr>
+          <tr>
+            <td>Colums context</td>
+            <td>{JSON.stringify(project.params.cols_context)}</td>
+          </tr>
+          <tr>
+            <td>Is test dataset</td>
+            <td>{JSON.stringify(project.params.test)}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button onClick={handleShow} className="delete-button mt-3">
+        Delete project now
+      </button>
+
+      <div>
+        <Modal show={show} onHide={actionDelete}>
+          <Modal.Header>
+            <Modal.Title>Delete the project</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Do you really want to delete this project</Modal.Body>
+          <Modal.Footer>
+            <button onClick={handleClose}>No</button>
+            <button onClick={actionDelete}>Delete</button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </>
+  );
+};
