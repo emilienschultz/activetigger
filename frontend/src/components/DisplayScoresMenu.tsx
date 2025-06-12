c74268872f360c04cbba88e95efae9e78d280bd0
Émilien Schultz <emilien.schultz@gmail.com>
2025-06-12 15:07:17 +0200

    change statistics computed

---
+++ frontend/src/components/DisplayScoresMenu.tsx
@@ -0,0 +1,46 @@
+import { FC, useState } from 'react';
+import { DisplayScores } from './DisplayScores';
+
+interface DisplayScoresMenuPropos {
+  scores: Record<string, Record<string, number>>;
+  modelName?: string;
+  displayTitle?: boolean;
+}
+
+export const DisplayScoresMenu: FC<DisplayScoresMenuPropos> = ({
+  scores,
+  modelName,
+  displayTitle,
+}) => {
+  const keys = Object.keys(scores || {});
+  const [currentScore, setCurrentScore] = useState<string>(keys[0] || '');
+
+  if (!scores || Object.keys(scores).length === 0) {
+    return <div>No scores available</div>;
+  }
+  return (
+    <div>
+      <label htmlFor="statistics">
+        Scores{' '}
+        <select
+          id="statistics"
+          value={currentScore}
+          onChange={(e) => setCurrentScore(e.target.value)}
+        >
+          {Object.entries(scores).map(([key]) => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+      </label>
+      {scores[currentScore] && (
+        <DisplayScores
+          title={displayTitle ? currentScore : null}
+          scores={scores[currentScore]}
+          modelName={modelName}
+        />
+      )}
+    </div>
+  );
+};
