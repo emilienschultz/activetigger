9a1f1813fc51717262cc1496f0d5ef05d86a4763
Émilien Schultz <emilien.schultz@gmail.com>
2025-06-23 17:30:03 +0200

    change CORS

--- api/activetigger/app/main.py
+++ api/activetigger/app/main.py
@@ -75,7 +75,7 @@ app.include_router(files.router)
 # allow multiple servers (avoir CORS error)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://emilienschultz.github.io/activetigger/", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
