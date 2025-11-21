89ed33ff63d3d158bc520576382040cd00d2c517
A.Morin <axelmorinmail@gmail.com>
2025-11-21 11:27:44 +0000

    score table style update

--- frontend/src/components/DisplayTableStatistics.tsx
+++ frontend/src/components/DisplayTableStatistics.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import { FC } from 'react';
 import { MLStatisticsModel } from '../types';
 
@@ -13,14 +14,45 @@ interface TableModel {
 }
 
 export const DisplayTableStatistics: FC<DisplayTableStatisticsProps> = ({ scores, title }) => {
+  console.log('Scores : ', scores);
   const table = scores.table ? (scores.table as unknown as TableModel) : null;
   const labels = (scores?.table?.index as unknown as string[]) || [];
   const colCount = table?.columns.length || 0;
 
+  const isLabelColumn = (colIndex: number, rowIndex: number, labels: string[]) => {
+    return (
+      colIndex < Object.entries(labels).length - 1 && rowIndex < Object.entries(labels).length - 1
+    );
+  };
+
+  const isTotalCell = (colIndex: number, rowIndex: number, labels: string[]) => {
+    return (
+      colIndex === Object.entries(labels).length - 1 ||
+      rowIndex === Object.entries(labels).length - 1
+    );
+  };
+
+  const isDiag = (colIndex: number, rowIndex: number, labels: string[]) => {
+    return colIndex < Object.entries(labels).length - 1 && colIndex === rowIndex;
+  };
+
+  const displayScore = (score: number | undefined) => {
+    if (typeof score === 'number') {
+      if (score === 0) {
+        return '0.00';
+      } else if (score === 1) {
+        return '1.00';
+      } else {
+        return (String(score) + '0').slice(0, 4);
+      }
+    }
+    return '';
+  };
+
   return (
-    <div className="overflow-x-auto">
+    <div id="DisplayTableStatistics">
       {table && (
-        <table className="table-auto border-collapse border border-gray-300 w-full text-sm">
+        <table>
           {title && (
             <caption className="caption-top text-lg font-medium mb-2 text-gray-700">
               {title}
@@ -28,32 +60,43 @@ export const DisplayTableStatistics: FC<DisplayTableStatisticsProps> = ({ scores
           )}
           <thead>
             <tr>
-              <th></th>
-              <th></th>
-              <td colSpan={Object.entries(labels).length} className="bg-gray-300 text-center p-2">
+              <td></td>
+              <td></td>
+              <td
+                colSpan={Object.entries(labels).length}
+                className="text-center p-2 border-bottom border-secondary"
+              >
                 Predicted
               </td>
-              <td colSpan={3} className="bg-gray-300 text-center p-2">
+              <td style={{ width: '20px' }}></td>
+              <td colSpan={2} className="text-center p-2 border-bottom border-secondary">
                 Scores
               </td>
             </tr>
             <tr className="bg-gray-100">
-              <td></td>
-              <td className="p-1">Label</td>
+              <th></th>
+              <th></th>
               {table?.columns.map((col, colIndex) => (
-                <td key={col} className="p-1 capitalize font-semibold">
-                  {colIndex === table?.columns.length - 1 ? col : <b>{col}</b>}
+                <td
+                  key={col}
+                  className={cx(
+                    'text-center ',
+                    colIndex === table?.columns.length - 1 ? '' : 'fw-bold',
+                  )}
+                >
+                  {col}
                 </td>
               ))}
-              <td className="p-1 text-center">Recall</td>
-              <td className="p-1 text-center">F1</td>
+              <th></th>
+              <th className="text-center fw-normal">Recall</th>
+              <th className="text-center fw-normal">F1</th>
             </tr>
           </thead>
           <tbody>
             {table.data.map((row, rowIndex) => (
               <tr key={table.index[rowIndex]}>
                 {rowIndex === 0 && (
-                  <td rowSpan={colCount} className="bg-blue-100 font-semibold text-center p-3">
+                  <td rowSpan={colCount} className="rowspan-cell">
                     Truth
                   </td>
                 )}
@@ -65,53 +108,62 @@ export const DisplayTableStatistics: FC<DisplayTableStatisticsProps> = ({ scores
                   )}
                 </td>
                 {row.map((cell, colIndex) => (
-                  <td key={colIndex} className="p-1 text-center">
-                    {(colIndex < row.length - 1 && rowIndex < table.data.length - 1) ||
-                    (rowIndex === row.length - 1 && colIndex === table.data.length - 1) ? (
-                      <b>{cell}</b>
-                    ) : (
-                      cell
+                  <td
+                    key={colIndex}
+                    className={cx(
+                      'cell',
+                      isLabelColumn(colIndex, rowIndex, labels) ? ' label-col' : '',
+                      isDiag(colIndex, rowIndex, labels) ? ' diag-cell' : '',
+                      isTotalCell(colIndex, rowIndex, labels) ? ' total-cell' : '',
                     )}
+                  >
+                    {cell}
                   </td>
                 ))}
+                <td></td>
 
-                <td className="p-1 text-center">
-                  {scores.recall_label && scores.recall_label[labels[rowIndex]]}
+                <td className="cell">
+                  {scores.recall_label && displayScore(scores.recall_label[labels[rowIndex]])}
                 </td>
-                <td className="p-1 text-center">
-                  {scores.f1_label && scores.f1_label[labels[rowIndex]]}
+                <td className="cell">
+                  {scores.f1_label && displayScore(scores.f1_label[labels[rowIndex]])}
                 </td>
               </tr>
             ))}
 
-            <tr>
+            <tr style={{ height: '20px' }}>
               <td></td>
-              <td className="p-1">Precision</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+
+            <tr>
+              <td rowSpan={2} className="rowspan-cell">
+                {' '}
+                Scores
+              </td>
+              <td>Precision</td>
               {table.columns.map((col, colIndex) => (
-                <td key={colIndex} className="p-1 text-center">
-                  {scores.precision_label && scores.precision_label[col]}
+                <td key={colIndex} className="cell">
+                  {scores.precision_label && displayScore(scores.precision_label[col])}
                 </td>
               ))}
+              <td></td>
+              <td></td>
+              <td></td>
             </tr>
             <tr>
-              <td></td>
-              <td className="p-1">F1</td>
+              <td>F1</td>
               {table.columns.map((col, colIndex) => (
-                <td key={colIndex} className="p-1 text-center">
-                  {scores.f1_label && scores.f1_label[col]}
+                <td key={colIndex} className="cell">
+                  {scores.f1_label && displayScore(scores.f1_label[col])}
                 </td>
               ))}
-            </tr>
-            <tr className="bg-gray-100">
               <td></td>
-              <td className="p-1"></td>
-              {table?.columns.slice(0, -1).map((col) => (
-                <td key={col} className="p-1 capitalize font-semibold">
-                  {col}
-                </td>
-              ))}
-              <td className="p-1 text-center"></td>
-              <td className="p-1 text-center"></td>
+              <td></td>
+              <td></td>
             </tr>
           </tbody>
         </table>
