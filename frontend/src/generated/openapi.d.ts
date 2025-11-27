1f400555c74e20ac949f8e458f77f3dae6b20597
A.Morin <axelmorinmail@gmail.com>
2025-11-27 16:43:02 +0100

    openapi update

--- frontend/src/generated/openapi.d.ts
+++ frontend/src/generated/openapi.d.ts
@@ -2038,7 +2038,10 @@ export interface components {
             grant_type?: string | null;
             /** Username */
             username: string;
-            /** Password */
+            /**
+             * Password
+             * Format: password
+             */
             password: string;
             /**
              * Scope
@@ -2047,7 +2050,10 @@ export interface components {
             scope: string;
             /** Client Id */
             client_id?: string | null;
-            /** Client Secret */
+            /**
+             * Client Secret
+             * Format: password
+             */
             client_secret?: string | null;
         };
         /** Body_upload_file_files_add_project_post */
@@ -2208,7 +2214,9 @@ export interface components {
             /** Text */
             text: string;
             /** Context */
-            context: Record<string, never>;
+            context: {
+                [key: string]: unknown;
+            };
             /** Selection */
             selection: string;
             /** Info */
@@ -2253,7 +2261,9 @@ export interface components {
             /** Name */
             name: string;
             /** Parameters */
-            parameters: Record<string, never>;
+            parameters: {
+                [key: string]: unknown;
+            };
             /** User */
             user: string;
             /** Time */
@@ -2279,7 +2289,9 @@ export interface components {
         FeaturesProjectStateModel: {
             /** Options */
             options: {
-                [key: string]: Record<string, never> | undefined;
+                [key: string]: {
+                    [key: string]: unknown;
+                } | undefined;
             };
             /** Available */
             available: string[];
@@ -2421,7 +2433,9 @@ export interface components {
             progress?: number | null;
             /** Loss */
             loss?: {
-                [key: string]: Record<string, never> | undefined;
+                [key: string]: {
+                    [key: string]: unknown;
+                } | undefined;
             } | null;
             /** Epochs */
             epochs?: number | null;
@@ -2502,7 +2516,9 @@ export interface components {
         /** LanguageModelsProjectStateModel */
         LanguageModelsProjectStateModel: {
             /** Options */
-            options: Record<string, never>[];
+            options: {
+                [key: string]: unknown;
+            }[];
             /** Available */
             available: {
                 [key: string]: {
@@ -2546,9 +2562,13 @@ export interface components {
             /** Confusion Matrix */
             confusion_matrix?: number[][] | null;
             /** False Predictions */
-            false_predictions?: Record<string, never> | unknown[] | null;
+            false_predictions?: {
+                [key: string]: unknown;
+            } | unknown[] | null;
             /** Table */
-            table?: Record<string, never> | null;
+            table?: {
+                [key: string]: unknown;
+            } | null;
         };
         /** MessagesInModel */
         MessagesInModel: {
@@ -2594,23 +2614,37 @@ export interface components {
         /** ModelInformationsModel */
         ModelInformationsModel: {
             /** Params */
-            params?: Record<string, never> | null;
+            params?: {
+                [key: string]: unknown;
+            } | null;
             /** Loss */
-            loss?: Record<string, never> | null;
+            loss?: {
+                [key: string]: unknown;
+            } | null;
             scores: components["schemas"]["ModelScoresModel"];
         };
         /** ModelScoresModel */
         ModelScoresModel: {
             /** Internalvalid Scores */
-            internalvalid_scores?: Record<string, never> | null;
+            internalvalid_scores?: {
+                [key: string]: unknown;
+            } | null;
             /** Train Scores */
-            train_scores?: Record<string, never> | null;
+            train_scores?: {
+                [key: string]: unknown;
+            } | null;
             /** Valid Scores */
-            valid_scores?: Record<string, never> | null;
+            valid_scores?: {
+                [key: string]: unknown;
+            } | null;
             /** Test Scores */
-            test_scores?: Record<string, never> | null;
+            test_scores?: {
+                [key: string]: unknown;
+            } | null;
             /** Outofsample Scores */
-            outofsample_scores?: Record<string, never> | null;
+            outofsample_scores?: {
+                [key: string]: unknown;
+            } | null;
         };
         /**
          * NextInModel
@@ -2800,7 +2834,9 @@ export interface components {
             /** Train Annotated N */
             train_annotated_n: number;
             /** Train Annotated Distribution */
-            train_annotated_distribution: Record<string, never>;
+            train_annotated_distribution: {
+                [key: string]: unknown;
+            };
             /** Test Set N */
             test_set_n?: number | null;
             /** Valid Set N */
@@ -2810,9 +2846,13 @@ export interface components {
             /** Valid Annotated N */
             valid_annotated_n?: number | null;
             /** Test Annotated Distribution */
-            test_annotated_distribution?: Record<string, never> | null;
+            test_annotated_distribution?: {
+                [key: string]: unknown;
+            } | null;
             /** Valid Annotated Distribution */
-            valid_annotated_distribution?: Record<string, never> | null;
+            valid_annotated_distribution?: {
+                [key: string]: unknown;
+            } | null;
             /** Sm 10Cv */
             sm_10cv?: unknown | null;
         };
@@ -3025,7 +3065,9 @@ export interface components {
         ProjectionsProjectStateModel: {
             /** Options */
             options: {
-                [key: string]: Record<string, never> | undefined;
+                [key: string]: {
+                    [key: string]: unknown;
+                } | undefined;
             };
             /** Available */
             available: {
@@ -3050,7 +3092,9 @@ export interface components {
             /** Text */
             text: string;
             /** Parameters */
-            parameters: Record<string, never>;
+            parameters: {
+                [key: string]: unknown;
+            };
         };
         /**
          * QuickModelInModel
@@ -3113,7 +3157,9 @@ export interface components {
         /** QuickModelsProjectStateModel */
         QuickModelsProjectStateModel: {
             /** Options */
-            options: Record<string, never>;
+            options: {
+                [key: string]: unknown;
+            };
             /** Available */
             available: {
                 [key: string]: components["schemas"]["ModelDescriptionModel"][] | undefined;
@@ -3180,11 +3226,17 @@ export interface components {
             };
             gpu: components["schemas"]["GpuInformationModel"];
             /** Cpu */
-            cpu: Record<string, never>;
+            cpu: {
+                [key: string]: unknown;
+            };
             /** Memory */
-            memory: Record<string, never>;
+            memory: {
+                [key: string]: unknown;
+            };
             /** Disk */
-            disk: Record<string, never>;
+            disk: {
+                [key: string]: unknown;
+            };
             /** Mail Available */
             mail_available: boolean;
             /** Messages */
@@ -5990,7 +6042,9 @@ export interface operations {
                 };
                 content: {
                     "application/json": {
-                        [key: string]: (unknown[] | Record<string, never>) | undefined;
+                        [key: string]: (unknown[] | {
+                            [key: string]: unknown;
+                        }) | undefined;
                     };
                 };
             };
