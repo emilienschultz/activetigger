4550114cf7b72d26b5196f8a170637460ecf2842
A.Morin <axelmorinmail@gmail.com>
2025-12-10 17:04:00 +0100

    training process wrapper

--- frontend/src/components/DisplayTrainingProcesses.tsx
+++ frontend/src/components/DisplayTrainingProcesses.tsx
@@ -56,42 +56,32 @@ export const DisplayTrainingProcesses: FC<DisplayTrainingProcessesProps> = ({
   return (
     <div className="overflow-x-auto">
       {Object.keys(processes || {}).length > 0 && displayStopButton && (
-        <div>
-          <button
-            key="stop"
-            className="btn btn-primary mt-3 d-flex align-items-center"
-            onClick={() => stopProcesses('bert')}
-          >
-            <PulseLoader color={'white'} /> Stop process
-          </button>
-        </div>
+        <button key="stop" className="btn-stop-process" onClick={() => stopProcesses('bert')}>
+          <PulseLoader color={'white'} /> Stop process
+        </button>
       )}
       {Object.keys(processes || {}).length > 0 && (
-        <div className="mt-3">
+        <div>
           Process running:
           <ul className="list-group">
             {Object.entries(
               processes as Record<string, Record<string, string | number | null>>,
             ).map(([user, v]) => (
               <li className="list-group-item" key={v.name}>
-                <div className="d-flex justify-content-between align-items-center">
-                  <div>
-                    <strong>From:</strong> {user} <br />
-                    <strong>Name:</strong> {v.name} <br />
-                    <strong>Status:</strong> {v.status}
-                  </div>
-                  <div className="text-end">
-                    <span className="fw-bold">{displayAdvancement(v.progress)}</span>
-                    {v.status === 'training' && (
-                      <div className="mt-2">
-                        <LossChart
-                          loss={v.loss as unknown as LossData}
-                          xmax={(v.epochs as number) || undefined}
-                        />
-                      </div>
-                    )}
-                  </div>
+                <div className="horizontal wrap" style={{ justifyContent: 'space-between' }}>
+                  <span style={{ marginRight: '10px' }}>From: {user};</span>
+                  <span style={{ marginRight: '10px' }}>Name: {v.name};</span>
+                  <span style={{ marginRight: '10px' }}>Status: {v.status};</span>
+                  <span style={{ marginRight: '10px' }} className="fw-bold">
+                    {displayAdvancement(v.progress)}
+                  </span>
                 </div>
+                {v.status === 'training' && (
+                  <LossChart
+                    loss={v.loss as unknown as LossData}
+                    xmax={(v.epochs as number) || undefined}
+                  />
+                )}
               </li>
             ))}
           </ul>
