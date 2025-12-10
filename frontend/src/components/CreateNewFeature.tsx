007f47e02289daa7d0b00ce113f17008de3628f1
A.Morin <axelmorinmail@gmail.com>
2025-12-10 16:05:22 +0100

    create a new feature form

--- frontend/src/components/CreateNewFeature.tsx
+++ frontend/src/components/CreateNewFeature.tsx
@@ -70,145 +70,136 @@ export const CreateNewFeature: FC<CreateNewFeatureProps> = ({
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(createNewFeature)} className="mt-3">
-        <label htmlFor="newFeature">Feature type</label>
-        <select id="newFeature" {...register('type')}>
-          <option key="empty"></option>
-          {Object.keys(featuresOption).map((element) => (
-            <option key={element} value={element}>
-              {element}
+    <form onSubmit={handleSubmit(createNewFeature)}>
+      <label htmlFor="newFeature">Feature type</label>
+      <select id="newFeature" {...register('type')}>
+        <option key="empty"></option>
+        {Object.keys(featuresOption).map((element) => (
+          <option key={element} value={element}>
+            {element}
+          </option>
+        ))}{' '}
+      </select>
+
+      <label htmlFor="name">Feature name</label>
+      <input
+        type="text"
+        placeholder="Enter the feature name"
+        {...register('name', { required: true })}
+      />
+
+      {selectedFeatureToCreate === 'sbert' && (
+        <details>
+          <summary>Advanced settings</summary>
+          <label htmlFor="model">Model to use</label>
+          <select id="model" {...register('parameters.model')}>
+            <option key={null} value="generic">
+              Default model
             </option>
-          ))}{' '}
-        </select>
-
-        <label htmlFor="name">Feature name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter the feature name"
-          {...register('name', { required: true })}
-        />
-
-        {selectedFeatureToCreate === 'sbert' && (
-          <details className="custom-details">
-            <summary>Advanced settings</summary>
-            <label htmlFor="model">Model to use</label>
-            <select id="model" {...register('parameters.model')}>
-              <option key={null} value="generic">
-                Default model
+            {(
+              (featuresOption.sbert ? (featuresOption['sbert']['models'] as string[]) : []) || []
+            ).map((element) => (
+              <option key={element as string} value={element as string}>
+                {element as string}
               </option>
-              {(
-                (featuresOption.sbert ? (featuresOption['sbert']['models'] as string[]) : []) || []
-              ).map((element) => (
-                <option key={element as string} value={element as string}>
-                  {element as string}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="length">Max length tokens</label>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Max length tokens"
-              {...register('parameters.max_length_tokens')}
-            />
-          </details>
-        )}
-
-        {selectedFeatureToCreate === 'fasttext' && (
-          <details className="custom-details">
-            <summary>Advanced settings</summary>
-            <label htmlFor="model">Model to use</label>
-            <select id="dataset_col" {...register('parameters.model')}>
-              <option key={null} value="generic">
-                Default model
-              </option>
-
-              {(featuresOption.fasttext?.models || []).map((element) => (
-                <option key={element as string} value={element as string}>
-                  {element as string}
-                </option>
-              ))}
-            </select>
-          </details>
-        )}
-
-        {selectedFeatureToCreate === 'regex' && (
+            ))}
+          </select>
+          <label htmlFor="length">Max length tokens</label>
           <input
-            type="text"
-            className="form-control mt-3"
-            placeholder="Enter the regex"
-            {...register('parameters.value')}
+            type="number"
+            placeholder="Max length tokens"
+            {...register('parameters.max_length_tokens')}
           />
-        )}
+        </details>
+      )}
 
-        {selectedFeatureToCreate === 'dfm' && (
-          <details className="custom-details">
-            <summary>Advanced settings</summary>
-            <div>
-              <label htmlFor="dfm_tfidf">TF-IDF</label>
-              <select id="dfm_tfidf" {...register('parameters.dfm_tfidf')}>
-                <option key="true">True</option>
-                <option key="false">False</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="dfm_ngrams">Ngrams</label>
-              <input type="number" id="dfm_ngrams" {...register('parameters.dfm_ngrams')} />
-            </div>
-            <div>
-              <label htmlFor="dfm_min_term_freq">Min term freq</label>
-              <input
-                type="number"
-                id="dfm_min_term_freq"
-                {...register('parameters.dfm_min_term_freq')}
-              />
-            </div>
-            <div>
-              <label htmlFor="dfm_max_term_freq">Max term freq</label>
-              <input
-                type="number"
-                id="dfm_max_term_freq"
-                {...register('parameters.dfm_max_term_freq')}
-              />
-            </div>
-            <div>
-              <label htmlFor="dfm_norm">Norm</label>
-              <select id="dfm_norm" {...register('parameters.dfm_norm')}>
-                <option key="true">True</option>
-                <option key="false">False</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="dfm_log">Log</label>
-              <select id="dfm_log" {...register('parameters.dfm_log')}>
-                <option key="true">True</option>
-                <option key="false">False</option>
-              </select>
-            </div>
-          </details>
-        )}
+      {selectedFeatureToCreate === 'fasttext' && (
+        <details>
+          <summary>Advanced settings</summary>
+          <label htmlFor="model">Model to use</label>
+          <select id="dataset_col" {...register('parameters.model')}>
+            <option key={null} value="generic">
+              Default model
+            </option>
 
-        {selectedFeatureToCreate === 'dataset' && (
+            {(featuresOption.fasttext?.models || []).map((element) => (
+              <option key={element as string} value={element as string}>
+                {element as string}
+              </option>
+            ))}
+          </select>
+        </details>
+      )}
+
+      {selectedFeatureToCreate === 'regex' && (
+        <input type="text" placeholder="Enter the regex" {...register('parameters.value')} />
+      )}
+
+      {selectedFeatureToCreate === 'dfm' && (
+        <details>
+          <summary>Advanced settings</summary>
           <div>
-            <label htmlFor="dataset_col">Column to use</label>
-            <select id="dataset_col" {...register('parameters.dataset_col')}>
-              {columns.map((element) => (
-                <option key={element as string} value={element as string}>
-                  {element as string}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="dataset_type">Type of the feature</label>
-            <select id="dataset_type" {...register('parameters.dataset_type')}>
-              <option key="numeric">Numeric</option>
-              <option key="categorical">Categorical</option>
+            <label htmlFor="dfm_tfidf">TF-IDF</label>
+            <select id="dfm_tfidf" {...register('parameters.dfm_tfidf')}>
+              <option key="true">True</option>
+              <option key="false">False</option>
             </select>
           </div>
-        )}
-        <button className="btn btn-primary w-25">Compute</button>
-      </form>
-    </div>
+          <div>
+            <label htmlFor="dfm_ngrams">Ngrams</label>
+            <input type="number" id="dfm_ngrams" {...register('parameters.dfm_ngrams')} />
+          </div>
+          <div>
+            <label htmlFor="dfm_min_term_freq">Min term freq</label>
+            <input
+              type="number"
+              id="dfm_min_term_freq"
+              {...register('parameters.dfm_min_term_freq')}
+            />
+          </div>
+          <div>
+            <label htmlFor="dfm_max_term_freq">Max term freq</label>
+            <input
+              type="number"
+              id="dfm_max_term_freq"
+              {...register('parameters.dfm_max_term_freq')}
+            />
+          </div>
+          <div>
+            <label htmlFor="dfm_norm">Norm</label>
+            <select id="dfm_norm" {...register('parameters.dfm_norm')}>
+              <option key="true">True</option>
+              <option key="false">False</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="dfm_log">Log</label>
+            <select id="dfm_log" {...register('parameters.dfm_log')}>
+              <option key="true">True</option>
+              <option key="false">False</option>
+            </select>
+          </div>
+        </details>
+      )}
+
+      {selectedFeatureToCreate === 'dataset' && (
+        <>
+          <label htmlFor="dataset_col">Column to use</label>
+          <select id="dataset_col" {...register('parameters.dataset_col')}>
+            {columns.map((element) => (
+              <option key={element as string} value={element as string}>
+                {element as string}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="dataset_type">Type of the feature</label>
+          <select id="dataset_type" {...register('parameters.dataset_type')}>
+            <option key="numeric">Numeric</option>
+            <option key="categorical">Categorical</option>
+          </select>
+        </>
+      )}
+      <button className="btn-submit">Compute</button>
+    </form>
   );
 };
