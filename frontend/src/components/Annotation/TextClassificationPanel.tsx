aa218a32492446e25f26f997e7662f8fa0dc0368
Paul Girard <paul@ouestware.com>
2025-12-19 16:34:34 +0100

    [frontend] Element context & history layout in annotation

--- frontend/src/components/Annotation/TextClassificationPanel.tsx
+++ frontend/src/components/Annotation/TextClassificationPanel.tsx
@@ -3,6 +3,7 @@ import Highlighter from 'react-highlight-words';
 import { DisplayConfig, ElementOutModel } from '../../types';
 
 import { FC, LegacyRef } from 'react';
+import { PiEmptyBold } from 'react-icons/pi';
 import { Tooltip } from 'react-tooltip';
 
 interface ClassificationPanelProps {
@@ -35,29 +36,16 @@ export const TextClassificationPanel: FC<ClassificationPanelProps> = ({
         style={{ minHeight: `${displayConfig.frameSize}vh`, height: '100%' }}
         ref={frameRef as unknown as LegacyRef<HTMLDivElement>}
       >
-        <motion.div
-          animate={elementId ? { backgroundColor: ['#ea6b1f70', '#ffffff00'] } : {}}
-          transition={{ duration: 1 }}
-        >
-          <Highlighter
-            highlightClassName="Search"
-            searchWords={validHighlightText}
-            autoEscape={false}
-            textToHighlight={textInFrame}
-            highlightStyle={{
-              backgroundColor: 'yellow',
-              margin: '0px',
-              padding: '2px 1px',
-            }}
-            caseSensitive={true}
-          />
-          {/* text out of frame */}
-          <span className="text-out-context" title="Outside context window ">
+        <p className="element-text">
+          <motion.div
+            animate={elementId ? { backgroundColor: ['#ea6b1f70', '#ffffff00'] } : {}}
+            transition={{ duration: 1 }}
+          >
             <Highlighter
               highlightClassName="Search"
               searchWords={validHighlightText}
               autoEscape={false}
-              textToHighlight={textOutFrame}
+              textToHighlight={textInFrame}
               highlightStyle={{
                 backgroundColor: 'yellow',
                 margin: '0px',
@@ -65,67 +53,67 @@ export const TextClassificationPanel: FC<ClassificationPanelProps> = ({
               }}
               caseSensitive={true}
             />
-          </span>
-          <Tooltip anchorSelect=".text-out-context" place="bottom" style={{ zIndex: 99 }}>
-            Outside context window.
-            <br />
-            Go to config menu to change context window according to the model you plan to use
-          </Tooltip>
-        </motion.div>
+            {/* text out of frame */}
+            <span className="text-out-context" title="Outside context window ">
+              <Highlighter
+                highlightClassName="Search"
+                searchWords={validHighlightText}
+                autoEscape={false}
+                textToHighlight={textOutFrame}
+                highlightStyle={{
+                  backgroundColor: 'yellow',
+                  margin: '0px',
+                  padding: '2px 1px',
+                }}
+                caseSensitive={true}
+              />
+            </span>
+            <Tooltip anchorSelect=".text-out-context" place="bottom" style={{ zIndex: 99 }}>
+              Outside context window.
+              <br />
+              Go to config menu to change context window according to the model you plan to use
+            </Tooltip>
+          </motion.div>
+        </p>
+
+        <div className="footer">
+          {
+            //display context
+            phase != 'test' && displayConfig.displayContext && (
+              <details>
+                <summary>Context </summary>
+                <dl>
+                  {Object.entries(element?.context || { None: 'None' }).map(([k, v]) => {
+                    return (
+                      <div className="dl-item" key={k}>
+                        <dt>{k}</dt>
+                        <dd>{v as string}</dd>
+                      </div>
+                    );
+                  })}
+                </dl>
+              </details>
+            )
+          }
+          {
+            //display history
+            phase != 'test' && displayConfig.displayHistory && (
+              <details>
+                <summary>History</summary>
+                <div>
+                  {((element?.history as string[]) || []).map((h, i) => {
+                    return (
+                      <div className="badge" key={i}>
+                        {h[0] || <PiEmptyBold />} ({h[2]})
+                      </div>
+                    );
+                  })}
+                </div>
+              </details>
+            )
+          }
+        </div>
       </div>
-      {/* {
-        //display proba
-        phase == 'train' && displayConfig.displayPrediction && element?.predict.label && (
-          <div className="d-flex mb-2 justify-content-center display-prediction">
-            <button
-              type="button"
-              key={element?.predict.label + '_predict'}
-              value={element?.predict.label as unknown as string}
-              className="btn btn-secondary"
-              onClick={(e) => {
-                postAnnotation(e.currentTarget.value, elementId);
-              }}
-            >
-              {displayPrediction(element)}
-            </button>
-          </div>
-        )
-      } */}
-      {
-        //display context
-        phase != 'test' && displayConfig.displayContext && (
-          <div>
-            Context{' '}
-            <ul>
-              {Object.entries(element?.context || { None: 'None' }).map(([k, v]) => {
-                return (
-                  <li key={k}>
-                    {k}: {v as string}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )
-      }
-      {
-        //display history
-        phase != 'test' && displayConfig.displayHistory && (
-          <div>
-            {/* History : {JSON.stringify(element?.history)} */}
-            History :{' '}
-            <ul>
-              {((element?.history as string[]) || []).map((h, i) => {
-                return (
-                  <li key={i}>
-                    {h[0]} — {h[2]}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )
-      }
     </>
   );
 };
