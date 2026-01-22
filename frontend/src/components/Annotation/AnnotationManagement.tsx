6307b1b2f60389b15a6e0eb247198deb3267e5c9
eschultz-ensae <emilien.schultz@ensae.fr>
2026-01-22 13:01:01 +0100

    modularize active learning

--- frontend/src/components/Annotation/AnnotationManagement.tsx
+++ frontend/src/components/Annotation/AnnotationManagement.tsx
@@ -1,13 +1,11 @@
-import { CSSProperties, FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { CSSProperties, FC, useCallback, useEffect, useRef, useState } from 'react';
 import { LuRefreshCw } from 'react-icons/lu';
 import { useNavigate, useParams } from 'react-router-dom';
 import {
   useAddAnnotation,
   useGetElementById,
   useGetNextElementId,
-  useRetrainQuickModel,
   useStatistics,
-  useTrainQuickModel,
 } from '../../core/api';
 import { useAppContext } from '../../core/context';
 import { ElementOutModel } from '../../types';
@@ -17,13 +15,13 @@ import { Modal } from 'react-bootstrap';
 import { useNotifications } from '../../core/notifications';
 import { useAnnotationSessionHistory } from '../../core/useHistory';
 import { isValidRegex } from '../../core/utils';
-import { ActiveLearningManagement } from '../ActiveLearningManagement';
 import { TagDisplayParameters } from '../TagDisplayParameters';
 import { DisplayProjection } from '../vizualisation/DisplayProjection';
 import { AnnotationHistoryList } from './AnnotationHistoryList';
 import { AnnotationModeForm } from './AnnotationMode';
 import { MulticlassInput } from './MulticlassInput';
 import { MultilabelInput } from './MultilabelInput';
+import { SelectActiveLearning } from './SelectActiveLearning';
 import { TextClassificationPanel } from './TextClassificationPanel';
 import { TextSpanPanel } from './TextSpanPanel';
 
@@ -37,7 +35,6 @@ export const AnnotationManagement: FC = () => {
     currentProject: project,
     selectionConfig,
     displayConfig,
-    freqRefreshQuickModel,
     activeModel,
     history,
     selectionHistory,
@@ -90,7 +87,7 @@ export const AnnotationManagement: FC = () => {
       : 'multiclass',
   );
 
-  // get statistics to display (TODO : try a way to avoid another request ?)
+  // get statistics to display
   const { statistics, reFetchStatistics } = useStatistics(
     projectName || null,
     currentScheme || null,
@@ -211,151 +208,8 @@ export const AnnotationManagement: FC = () => {
   // Now filter by valid regex
   const validHighlightText = highlightText.filter(isValidRegex);
 
-  // existing models
-  const availableQuickModels = useMemo(
-    () => project?.quickmodel.available[currentScheme || ''] || [],
-    [project?.quickmodel, currentScheme],
-  );
-  const availableBertModels = useMemo(
-    () => project?.languagemodels.available[currentScheme || ''] || {},
-    [project?.languagemodels, currentScheme],
-  );
-  const availableBertModelsWithPrediction = Object.entries(availableBertModels || {})
-    .filter(([_, v]) => v && v.predicted)
-    .map(([k, _]) => k);
-  //
-  // TODO only keep those with prediction
-  const groupedModels = [
-    {
-      label: 'Quick Models',
-      options: (availableQuickModels ?? [])
-        .filter((e) => e?.name) // <-- protect against undefined/missing name
-        .map((e) => ({
-          value: e.name,
-          label: e.name,
-          type: 'quickmodel',
-        })),
-    },
-    {
-      label: 'Language Models',
-      options: (availableBertModelsWithPrediction ?? [])
-        .filter((e) => e) // <-- ensure non-null
-        .map((e) => ({
-          value: e,
-          label: e,
-          type: 'languagemodel',
-        })),
-    },
-  ];
-
   // display active menu
   const [activeMenu, setActiveMenu] = useState<boolean>(false);
-
-  // train a quick model
-  const { trainQuickModel } = useTrainQuickModel(projectName || null, currentScheme || null);
-  const startTrainQuickModel = () => {
-    // default quickmodel
-    const availableFeatures = project?.features.available ? project?.features.available : [];
-    if (availableFeatures.length === 0) {
-      setActiveMenu(false);
-      notify({
-        type: 'warning',
-        message: 'No features available for quickmodel',
-      });
-    }
-    const formData = {
-      name: 'basic-quickmodel',
-      model: 'logistic-l1',
-      scheme: currentScheme || '',
-      params: {
-        costLogL2: 1,
-        costLogL1: 1,
-        n_neighbors: 3,
-        alpha: 1,
-        n_estimators: 500,
-        max_features: null,
-      },
-      dichotomize: null,
-      features: availableFeatures,
-      cv10: false,
-      standardize: false,
-      balance_classes: false,
-    };
-    trainQuickModel(formData);
-    setActiveMenu(false);
-    setSelectFirstModelTrained(true);
-  };
-
-  // fastrack active learning model
-  useEffect(() => {
-    if (selectFirstModelTrained && availableQuickModels.length > 0) {
-      // select the first trained model
-      setAppContext((prev) => ({
-        ...prev,
-        activeModel: {
-          type: 'quickmodel',
-          value: availableQuickModels[0].name,
-          label: availableQuickModels[0].name,
-        },
-        selectionConfig: { ...prev.selectionConfig, mode: 'active' },
-      }));
-    }
-  }, [availableQuickModels, selectFirstModelTrained, setAppContext]);
-
-  // retrain quick model (only for )
-  const { retrainQuickModel } = useRetrainQuickModel(projectName || null, currentScheme || null);
-  const [updatedQuickModel, setUpdatedQuickModel] = useState(false);
-  useEffect(() => {
-    // only the training points for the current phase
-    const trainHistory = history.filter(
-      (hp) => hp.dataset === 'train' && hp.project_slug === projectName,
-    );
-    if (
-      !updatedQuickModel &&
-      freqRefreshQuickModel &&
-      activeModel &&
-      trainHistory.length > 0 &&
-      trainHistory.length % freqRefreshQuickModel == 0 &&
-      activeModel.type === 'quickmodel'
-    ) {
-      setUpdatedQuickModel(true);
-      retrainQuickModel(activeModel.value);
-    }
-    if (
-      updatedQuickModel &&
-      freqRefreshQuickModel &&
-      trainHistory.length % freqRefreshQuickModel != 0
-    ) {
-      setUpdatedQuickModel(false);
-    }
-  }, [
-    freqRefreshQuickModel,
-    setUpdatedQuickModel,
-    activeModel,
-    updatedQuickModel,
-    retrainQuickModel,
-    history.length,
-    projectName,
-    history,
-  ]);
-
-  // deactivate active model if it has been removed from available models
-  useEffect(() => {
-    if (
-      activeModel &&
-      !availableQuickModels.find((model) => model.name === activeModel.value) &&
-      activeModel.type === 'quickmodel'
-    ) {
-      setAppContext((prev) => ({ ...prev, activeModel: null }));
-    }
-    if (
-      activeModel &&
-      !Object.keys(availableBertModels).includes(activeModel.value) &&
-      activeModel.type === 'languagemodel'
-    ) {
-      setAppContext((prev) => ({ ...prev, activeModel: null }));
-    }
-  }, [availableQuickModels, activeModel, setAppContext, availableBertModels]);
 
   /**
    * Update element if selectionConfig changed :
@@ -394,6 +248,10 @@ export const AnnotationManagement: FC = () => {
   }, [selectionConfig]);
 
   if (!projectName || !currentScheme) return;
+
+  const numberAnnotated = history.filter(
+    (hp) => hp.dataset === 'train' && hp.project_slug === projectName,
+  );
 
   return (
     <>
@@ -502,7 +360,16 @@ export const AnnotationManagement: FC = () => {
           </span>
         )}
       </div>
-
+      {/**
+       * Manage active learning
+       **/}
+      <SelectActiveLearning
+        display={activeMenu}
+        setActiveMenu={setActiveMenu}
+        setSelectFirstModelTrained={setSelectFirstModelTrained}
+        selectFirstModelTrained={selectFirstModelTrained}
+        numberAnnotated={numberAnnotated.length}
+      />
       <Modal show={showDisplayViz} onHide={handleCloseViz} size="xl" id="viz-modal">
         <Modal.Header closeButton>
           <Modal.Title>Current projection</Modal.Title>
@@ -523,32 +390,6 @@ export const AnnotationManagement: FC = () => {
         </Modal.Header>
         <Modal.Body>
           <TagDisplayParameters />
-        </Modal.Body>
-      </Modal>
-      <Modal show={activeMenu} onHide={() => setActiveMenu(false)} id="active-modal" size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title>Configure active learning</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {availableQuickModels.length > 0 ? (
-            <ActiveLearningManagement
-              availableModels={groupedModels}
-              setAppContext={setAppContext}
-              freqRefreshQuickModel={freqRefreshQuickModel}
-              activeModel={activeModel}
-              projectName={projectName}
-              currentScheme={currentScheme}
-            />
-          ) : (
-            <>
-              <div className="horizontal center">
-                No quick model currently available. Go to model tab or
-              </div>
-              <button className="btn-submit" onClick={startTrainQuickModel}>
-                Train a default quick model
-              </button>
-            </>
-          )}
         </Modal.Body>
       </Modal>
     </>
---
+++ frontend/src/components/Annotation/SelectActiveLearning.tsx
@@ -0,0 +1,200 @@
+import { FC, useEffect, useMemo, useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import { useRetrainQuickModel, useTrainQuickModel } from '../../core/api';
+import { useAppContext } from '../../core/context';
+import { useNotifications } from '../../core/notifications';
+import { ActiveLearningManagement } from '../ActiveLearningManagement';
+
+interface SelectActiveLearningProps {
+  display: boolean;
+  setActiveMenu: (value: boolean) => void;
+  setSelectFirstModelTrained?: (value: boolean) => void;
+  selectFirstModelTrained?: boolean;
+  numberAnnotated?: number;
+}
+
+export const SelectActiveLearning: FC<SelectActiveLearningProps> = ({
+  display,
+  setActiveMenu,
+  setSelectFirstModelTrained,
+  selectFirstModelTrained,
+  numberAnnotated = 0,
+}) => {
+  const { notify } = useNotifications();
+
+  const {
+    appContext: { freqRefreshQuickModel, activeModel, currentScheme, currentProject: project },
+    setAppContext,
+  } = useAppContext();
+
+  const projectName = project?.params.project_name;
+
+  // existing models
+  const availableQuickModels = useMemo(
+    () => project?.quickmodel.available[currentScheme || ''] || [],
+    [project?.quickmodel, currentScheme],
+  );
+  const availableBertModels = useMemo(
+    () => project?.languagemodels.available[currentScheme || ''] || {},
+    [project?.languagemodels, currentScheme],
+  );
+
+  const availableBertModelsWithPrediction = Object.entries(availableBertModels || {})
+    .filter(([_, v]) => v && v.predicted)
+    .map(([k, _]) => k);
+
+  // TODO only keep those with prediction
+  const groupedModels = [
+    {
+      label: 'Quick Models',
+      options: (availableQuickModels ?? [])
+        .filter((e) => e?.name) // <-- protect against undefined/missing name
+        .map((e) => ({
+          value: e.name,
+          label: e.name,
+          type: 'quickmodel',
+        })),
+    },
+    {
+      label: 'Language Models',
+      options: (availableBertModelsWithPrediction ?? [])
+        .filter((e) => e) // <-- ensure non-null
+        .map((e) => ({
+          value: e,
+          label: e,
+          type: 'languagemodel',
+        })),
+    },
+  ];
+
+  const { trainQuickModel } = useTrainQuickModel(projectName || null, currentScheme || null);
+  const startTrainQuickModel = () => {
+    // default quickmodel
+    const availableFeatures = project?.features.available ? project?.features.available : [];
+    if (availableFeatures.length === 0) {
+      setActiveMenu(false);
+      notify({
+        type: 'warning',
+        message: 'No features available for quickmodel',
+      });
+    }
+    const formData = {
+      name: 'basic-quickmodel',
+      model: 'logistic-l1',
+      scheme: currentScheme || '',
+      params: {
+        costLogL2: 1,
+        costLogL1: 1,
+        n_neighbors: 3,
+        alpha: 1,
+        n_estimators: 500,
+        max_features: null,
+      },
+      dichotomize: null,
+      features: availableFeatures,
+      cv10: false,
+      standardize: false,
+      balance_classes: false,
+    };
+    trainQuickModel(formData);
+    setActiveMenu(false);
+    if (setSelectFirstModelTrained) setSelectFirstModelTrained(true);
+  };
+
+  // deactivate active model if it has been removed from available models
+  useEffect(() => {
+    if (
+      activeModel &&
+      !availableQuickModels.find((model) => model.name === activeModel.value) &&
+      activeModel.type === 'quickmodel'
+    ) {
+      setAppContext((prev) => ({ ...prev, activeModel: null }));
+    }
+    if (
+      activeModel &&
+      !Object.keys(availableBertModels).includes(activeModel.value) &&
+      activeModel.type === 'languagemodel'
+    ) {
+      setAppContext((prev) => ({ ...prev, activeModel: null }));
+    }
+  }, [availableQuickModels, activeModel, setAppContext, availableBertModels]);
+
+  // fastrack active learning model
+  useEffect(() => {
+    if (selectFirstModelTrained && availableQuickModels.length > 0) {
+      // select the first trained model
+      setAppContext((prev) => ({
+        ...prev,
+        activeModel: {
+          type: 'quickmodel',
+          value: availableQuickModels[0].name,
+          label: availableQuickModels[0].name,
+        },
+        selectionConfig: { ...prev.selectionConfig, mode: 'active' },
+      }));
+    }
+  }, [availableQuickModels, selectFirstModelTrained, setAppContext]);
+
+  // retrain quick model (only for )
+  const { retrainQuickModel } = useRetrainQuickModel(projectName || null, currentScheme || null);
+  const [updatedQuickModel, setUpdatedQuickModel] = useState(false);
+  useEffect(() => {
+    // only the training points for the current phase
+
+    if (
+      !updatedQuickModel &&
+      freqRefreshQuickModel &&
+      activeModel &&
+      numberAnnotated > 0 &&
+      numberAnnotated % freqRefreshQuickModel == 0 &&
+      activeModel.type === 'quickmodel'
+    ) {
+      setUpdatedQuickModel(true);
+      retrainQuickModel(activeModel.value);
+    }
+    if (
+      updatedQuickModel &&
+      freqRefreshQuickModel &&
+      numberAnnotated % freqRefreshQuickModel != 0
+    ) {
+      setUpdatedQuickModel(false);
+    }
+  }, [
+    freqRefreshQuickModel,
+    setUpdatedQuickModel,
+    activeModel,
+    updatedQuickModel,
+    retrainQuickModel,
+    numberAnnotated,
+    projectName,
+  ]);
+
+  return (
+    <Modal show={display} onHide={() => setActiveMenu(false)} id="active-modal" size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>Configure active learning</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {availableQuickModels.length > 0 ? (
+          <ActiveLearningManagement
+            availableModels={groupedModels}
+            setAppContext={setAppContext}
+            freqRefreshQuickModel={freqRefreshQuickModel}
+            activeModel={activeModel}
+            projectName={projectName || ''}
+            currentScheme={currentScheme || ''}
+          />
+        ) : (
+          <>
+            <div className="horizontal center">
+              No quick model currently available. Go to model tab or
+            </div>
+            <button className="btn-submit" onClick={startTrainQuickModel}>
+              Train a default quick model
+            </button>
+          </>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+};
