2355e44cffdff2c1f5c901fc474edd19f9cb6282
A.Morin <axelmorinmail@gmail.com>
2026-01-26 13:06:57 +0100

    prevent content from overflowing

--- frontend/src/components/layout/ProjectPageLayout.tsx
+++ frontend/src/components/layout/ProjectPageLayout.tsx
@@ -52,7 +52,7 @@ export const ProjectPageLayout: FC<ProjectPageLayoutProps> = ({
             currentMode={phase}
             developmentMode={developmentMode}
           />
-          <div className="flex-grow-1 mb-3 position-relative">{children}</div>
+          <div className="flex-grow-1 mb-3 position-relative overflow-auto">{children}</div>
         </div>
       </div>
       <StatusNotch
