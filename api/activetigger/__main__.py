fec918a76ba4b1671ae65c95f12cf1b636ba470d
eschultz-ensae <emilien.schultz@ensae.fr>
2026-02-06 19:03:01 +0100

    fix reload

--- api/activetigger/__main__.py
+++ api/activetigger/__main__.py
@@ -25,4 +25,5 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=args.portapi,
         reload=config.mode == "dev",
+        reload_excludes=["*/projects/*", "*/checkpoint-*"],
     )
