35b481a75b8add02de59974e2f4353073bfce7f1
eschultz-ensae <emilien.schultz@ensae.fr>
2026-03-13 20:31:37 +0100

    add stop button for features

--- frontend/src/components/StopProcessButton.tsx
+++ frontend/src/components/StopProcessButton.tsx
@@ -4,12 +4,13 @@ import { useStopProcesses } from '../core/api';
 
 interface stopProcessButtonProps {
   projectSlug: string | null;
+  kind?: string;
 }
 
-export const StopProcessButton: FC<stopProcessButtonProps> = ({ projectSlug }) => {
+export const StopProcessButton: FC<stopProcessButtonProps> = ({ projectSlug, kind = 'bert' }) => {
   const { stopProcesses } = useStopProcesses(projectSlug);
   return (
-    <button key="stop" className="btn-stop-process my-2" onClick={() => stopProcesses('bert')}>
+    <button key="stop" className="btn-stop-process my-2" onClick={() => stopProcesses(kind)}>
       <PulseLoader color={'white'} className="mx-2" /> Stop
     </button>
   );
